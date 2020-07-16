aaca4f2f30ebe2a8e05badb994900c56847b1878
duplxey <nick.tomazic@gmail.com>
2020-07-16 16:55:34 +0200

    Added proceed option.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import os
+from shutil import copyfile
+import ntpath
 
 allowed_extensions = [
     "png",
@@ -11,7 +13,7 @@ to_dir = input("To (absolute path): ")
 
 print("Searching for files...")
 
-found_files = set()
+matching_files = set()
 all_files_amount = 0
 
 for subdir, dirs, files in os.walk(from_dir):
@@ -21,9 +23,22 @@ for subdir, dirs, files in os.walk(from_dir):
         all_files_amount += 1
         if (file_extension.replace(".", "")) in allowed_extensions:
             print(filename + " > " + file_extension + " (" + full_path + ")")
-            found_files.add(full_path)
+            matching_files.add(full_path)
 
-if len(found_files) == 0:
+if len(matching_files) == 0:
     exit("Could not find any files.")
 
-print("Found", len(found_files), "matching files files (out of", all_files_amount, ").")
+print("Found", len(matching_files), "matching files files (out of", all_files_amount, ").")
+
+proceed = input("Press 'y' to copy the files: ")
+
+if proceed != 'y':
+    print("Cancelling the operation.")
+
+print("Started the copy process...")
+
+for matching_file in matching_files:
+    print("Copying " + matching_file)
+    copyfile(matching_file, to_dir + ntpath.basename(matching_file))
+
+print("Copy process completed.")
